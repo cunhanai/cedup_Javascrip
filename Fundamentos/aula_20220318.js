de04e2d588a9b7b6e8a5a3db3c3adeca8a5f812d
cunhanai <101332630+cunhanai@users.noreply.github.com>
2022-03-25 17:41:02 -0300

    Add files via upload

---
+++ Fundamentos/aula_20220318.js
@@ -0,0 +1,14 @@
+let nome = "Lucas"
+let array = "Ana, Maria, Pedro"
+
+console.log("Lucas".charAt(3))
+
+console.log(nome.indexOf('a'))
+
+console.log(nome.substring(2))
+
+console.log(nome.substring(5))
+
+console.log("Ana, Maria, Pedro".split(", "))
+
+console.log(array.split(", "))
---
+++ Fundamentos/aula_20220325.js
@@ -0,0 +1,10 @@
+const nome = 'Lucas'
+const sobrenome = ' Ferraz'
+const template = `Olá ${nome} ${sobrenome}`
+
+console.log(template)
+
+console.log(nome + sobrenome)
+
+const up = texto => texto.toUpperCase()
+console.log(`EI... ${up('cuidado')}`)
